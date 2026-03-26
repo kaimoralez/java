6e2c1b2078e1b09860a31a0a83e8debf9302adb0
kaimoralez <kapconc2009@gmail.com>
2026-03-26 00:26:39 -0300

    feat: adiciona exercício 1006 beecrowd

---
+++ exercicios/beecrowd/1006/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double notaA = scanner.nextDouble();
+        double notaB = scanner.nextDouble();
+        double notaC = scanner.nextDouble();
+
+        double media = (notaA * 2 + notaB * 3 + notaC * 5) / 10;
+
+        System.out.printf("MEDIA = %.1f\n", media);
+
+    }
+}
