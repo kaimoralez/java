aa9a0325f536b3406e9130986e9f027ad531ce76
kaimoralez <117987584+kaimoralez@users.noreply.github.com>
2026-05-11 14:25:28 -0300

    Implement password validation with attempts limit

--- cursos/senai/logica-de-programacao/estrutura_de_repeticao_I/1_caixa_eletronico/Main.java
+++ cursos/senai/logica-de-programacao/estrutura_de_repeticao_I/1_caixa_eletronico/Main.java
@@ -1,1 +1,28 @@
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite sua senha: ");
+
+        String senha = "4321";
+
+        for (int i = 0; i < 3; i++) {
+            String tentativas = scanner.nextLine().trim();
+
+            if (tentativas.equals(senha)) {
+                System.out.print("Senha valida!");
+                break;
+            } else {
+                if (i == 2) {
+                    System.out.print("Conta bloqueada");
+                } else {
+                    System.out.print("Senha invalida!Tente novamente:");
+                }
+            }
+
+        }
+
+    }
+}
