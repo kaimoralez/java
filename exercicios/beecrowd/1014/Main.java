92b955675734e9432c605e10fe1ab8b91250d79a
kaimoralez <117987584+kaimoralez@users.noreply.github.com>
2026-05-31 21:56:38 -0300

    Add Main.java for fuel consumption calculation

---
+++ exercicios/beecrowd/1014/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner (System.in);
+
+        int x = scanner.nextInt();
+        double y = scanner.nextDouble();
+        double total = x/y;
+
+        System.out.printf("%.3f km/l\n", total);
+
+        scanner.close();
+    }
+}
