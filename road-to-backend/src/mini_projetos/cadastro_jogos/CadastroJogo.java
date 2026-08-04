42719b99ab047da5823f399533a953be1e1b2bc0
kaimoralez <kapconc2009@gmail.com>
2026-08-03 23:55:40 -0300

    feat:criação do método adicionar

--- road-to-backend/src/mini_projetos/cadastro_jogos/CadastroJogo.java
+++ road-to-backend/src/mini_projetos/cadastro_jogos/CadastroJogo.java
@@ -1,4 +1,17 @@
 package mini_projetos.cadastro_jogos;
 
+import java.util.ArrayList;
+
 public class CadastroJogo {
+
+    private ArrayList<Jogo> jogos;
+
+    public CadastroJogo(){
+        jogos = new ArrayList<>();
+    }
+
+    public void adicionar(Jogo jogo){
+        jogos.add(jogo);
+    }
+
 }
