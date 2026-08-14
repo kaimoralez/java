11cff750342c18dea1f388fe0ac9b96ae2a3fa1a
kaimoralez <kapconc2009@gmail.com>
2026-08-13 23:09:39 -0300

    fetch: importacao da classe menu

--- road-to-backend/src/mini_projetos/cadastro_jogos/Main.java
+++ road-to-backend/src/mini_projetos/cadastro_jogos/Main.java
@@ -1,7 +1,11 @@
 package mini_projetos.cadastro_jogos;
 
+import java.util.Scanner;
+
 public class Main {
     static void main() {
 
+        Menu menu = new Menu();
+        menu.exibirMenu();
     }
 }
