4a3a84b571c5b35f4b50968fd4ebc42bc9334eb6
Kaique Pereira <117987584+kaimoralez@users.noreply.github.com>
2026-08-18 15:11:26 -0300

    feat: Add Main class for Tamagotchi application

---
+++ cursos/senai/backend/src/atividades/atv07/Main.java
@@ -0,0 +1,25 @@
+package atv08;
+
+import java.util.Scanner;
+
+public class Main {
+
+    static void main() {
+        Tamagotchi t;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Insira o nome do seu pet: ");
+
+        String nome = scanner.nextLine();
+
+        t = new Tamagotchi(nome);
+        System.out.println(t.getNome());
+        t.setFome(20);
+        t.setEnergia(80);
+        System.out.println(t.getHumor());
+
+
+    }
+
+
+}
