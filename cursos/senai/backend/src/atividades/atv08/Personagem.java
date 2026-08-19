bda06d8f8671a44da56b399b04c1de21aacd83f8
kaimoralez <kapconc2009@gmail.com>
2026-08-18 21:47:20 -0300

    feat: adiciona validações de vida e energia no construtor de Personagem

--- cursos/senai/backend/src/atividades/atv08/Personagem.java
+++ cursos/senai/backend/src/atividades/atv08/Personagem.java
@@ -1,4 +1,6 @@
-package atv09;
+package atv08;
+
+import static java.lang.Integer.parseInt;
 
 public class Personagem {
     private String nome;
@@ -7,32 +9,62 @@ public class Personagem {
     private int nivel;
 
     public Personagem(String nome, int vida, int energia) {
+
         this.nome = nome;
-        this.vida = vida;
-        this.energia = energia;
-        nivel = 1;
-    }
 
-    public void setEnergia(int energia) {
-        if (energia < 100 && energia > 0) {
-            this.energia = energia;
-        }
-    }
-
-    public void setVida(int vida) {
-        if (vida > 0 && vida < 100) {
+        if (vida < 0) {
+            this.vida = 0;
+        }else if (vida > 100){
+            this.vida = 100;
+        }else{
             this.vida = vida;
         }
+
+        if (energia < 0) {
+            this.energia = 0;
+        }else if (energia > 100){
+            this.energia = 100;
+        }else{
+            this.energia = energia;
+        }
+
+        nivel = 1;
     }
 
     public int receberDano(int dano) {
         if(dano < vida){
             return vida = vida - dano;
         }else{
-            return 2;
+            return vida = 0;
         }
     }
 
+    public int descansar(int descansa){
+        if ((energia + descansa) <= 100){
+            return energia = energia + descansa;
+        }else{
+            return energia = 100;
+        }
+    }
+
+    public int atacar (int ataque){
+        double totalAtaque = 0.5 * ataque;
+
+        if(energia == 0){
+            return 0;
+        }else{
+            return energia = energia - (int)totalAtaque;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "=======  Personagem  ======" +
+                " nome = " + nome  +
+                ", vida = " + vida +
+                ", energia = " + energia +
+                ", nivel = " + nivel;
+    }
 }
 
 
