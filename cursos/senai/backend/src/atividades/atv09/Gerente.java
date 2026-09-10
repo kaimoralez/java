28b2dce785ae70ef6c50cd013c55297fc2312fae
kaimoralez <kapconc2009@gmail.com>
2026-09-09 23:21:39 -0300

    feat: create manager class

---
+++ cursos/senai/backend/src/atividades/atv09/Gerente.java
@@ -0,0 +1,17 @@
+package atividades.atv09;
+
+public class Gerente extends Funcionario {
+    public Gerente(String nome, String cpf, double salario){
+        super (nome,cpf,salario);
+    }
+
+    @Override
+    public double calcularBonificacao() {
+        return getSalario() * 0.20;
+    }
+
+    @Override
+    public String cargo() {
+        return "Gerente";
+    }
+}
